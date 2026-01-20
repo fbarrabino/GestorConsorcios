67b66ded7265bb07321b939192c1622effc362e0
Franco Barrabino <barrabinof299@gmail.com>
2026-01-20 09:57:04 -0300

    Configuracion inicial de Spring Boot y conexion a DB

---
+++ src/main/java/consorcio/GestorConsorciosApplication.java
@@ -0,0 +1,13 @@
+package consorcio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorConsorciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorConsorciosApplication.class, args);
+	}
+
+}
